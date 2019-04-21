53cbb53c03d62fcda9b2dc63ec03194e61997f8f
Supason Kotanut <supasonk@gmail.com>
2019-04-21 23:12:26 +0700

    initialize boilerplate

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+// pages/about.js
+
+import Layout from '../components/MyLayout.js'
+
+export default function About() {
+  return (
+    <Layout>
+      <p>This is the about page</p>
+    </Layout>
+  )
+}
